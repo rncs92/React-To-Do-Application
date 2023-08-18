f6c54362636ee4433d69fa6402723cb703c00a7e
rncs92 <123461096+rncs92@users.noreply.github.com>
2023-08-18 15:13:01 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -143,6 +143,13 @@ function App() {
                       <input
                         type="text"
                         onBlur={(event) => updateTodo(event, todo.id)}
+                        onKeyDown={event => {
+                          if(event.key === 'Enter') {
+                            updateTodo(event, todo.id);
+                          } else if (event.key === 'Escape') {
+                            editingTodo(todo.id);
+                          }   
+                        }}
                         defaultValue={todo.title}
                         className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 w-full focus:border-blue-500 p-2.5"
                         autoFocus
